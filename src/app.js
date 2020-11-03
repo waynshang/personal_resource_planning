ba3201ed36b34f0501480024965c78775af29d45
wayneshang <wayne18308@gmail.com>
2020-11-03 23:11:11 +0800

    update url

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@ import SignInPage from './pages/logInPage'
 import Dashboard from './pages/dashBoardPage'
 // import { useParams} from "react-router";
 // import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { useRouteMatch } from "react-router-dom";
+import { useRouteMatch, useHistory } from "react-router-dom";
 
 
 function App(){
     const isLogin = useRouteMatch("/warranty_timeline/logIn");
     const isTimeLineList = useRouteMatch("/warranty_timeline/timeLineList");
-    const isDashBoard = useRouteMatch("/warranty_timeline/dashboard");
+    const isDashBoard = useRouteMatch("/warranty_timeline");
     return (
     <div className='app' >
         {isTimeLineList && <TimeLineList/>}
